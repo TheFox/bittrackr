480a4258d72d9975779325e648022b33337473ac
Christian Mayer <christian@fox21.at>
2024-04-29 18:03:22 +0200

    Main Implementation

--- src/bittrackr.py
+++ src/bittrackr.py
@@ -8,6 +8,7 @@ from json import loads
 from time import sleep
 from cmc import get_quotes as cmc_get_quotes
 from sty import fg, bg, ef, rs
+from datetime import datetime
 
 CLEAR_SCREEN='\033[2J'
 JUMP_BEGINNING='\033[1;1H'
@@ -21,11 +22,12 @@ def _m(i: int) -> str:
 
 class App():
     config: dict
+    _max_updates: int
     running: bool
     data: dict
     screen: dict
 
-    def __init__(self, config_path: str|None):
+    def __init__(self, config_path: str|None, update_interval: int|None = None, max_updates: int|None = None):
         print(f'-> config path: {config_path}')
         if config_path is None:
             self.config = self._default_config()
@@ -39,16 +41,30 @@ class App():
                 **config,
             }
 
+        if update_interval is not None:
+            self.config['update_interval'] = update_interval
+
+        self._max_updates = max_updates
+
         self.data = {}
         for sym in self.config['symbols']:
             self.data[sym] = {
                 'symbol': sym,
-                'last_updated': None,
-                'quote_price': None,
-                'prev_price': None,
                 'direction': 0,
-                'history': ['-'] * self.config['history_length'],
+                'history': [],
+                'prev_price': None,
+                'prev_volume_24h': None,
+                'diff_volume_24h': 0.0,
+                'dp': {
+                    'quote_price': None,
+                    'last_updated': None,
+                    'volume_24h': None,
+                    'volume_change_24h': None,
+                    'percent_change_24h': None,
+                },
             }
+            if self.config['history']['enabled']:
+                self.data[sym]['history'] = ['-'] * self.config['history']['length']
 
         terminal = shutil.get_terminal_size((80, 20))
         self.screen = {
@@ -68,12 +84,20 @@ class App():
         # Jump to top left.
         print(JUMP_BEGINNING, end='', flush=True)
 
+        cycle_n = 0
         while self.running:
+            cycle_n += 1
+
             self._data_update()
             self._screen_update()
 
+            if self._max_updates is not None and cycle_n >= self._max_updates:
+                self.shutdown('max cycles reached')
+                break
+
+            print(f'update_interval={self.config["update_interval"]} cycle={cycle_n} mu={self._max_updates}')
             for n in sleep_list:
-                print(f'  next update in {n} ({self.config["update_interval"]})    \r', end='', flush=True)
+                print(f'  next update in {n}    \r', end='', flush=True)
                 sleep(1)
                 if not self.running:
                     break
@@ -92,30 +116,42 @@ class App():
                     fsdata = sdata[0]
                     fiat_quote = fsdata['quote'][self.config['convert']]
 
-                    self.data[sym]['quote_price'] = fiat_quote['price']
-                    self.data[sym]['last_updated'] = fiat_quote['last_updated']
+                    self.data[sym]['dp']['quote_price'] = fiat_quote['price']
+                    self.data[sym]['dp']['last_updated'] = fiat_quote['last_updated']
+                    self.data[sym]['dp']['volume_24h'] = fiat_quote['volume_24h']
+                    self.data[sym]['dp']['volume_change_24h'] = fiat_quote['volume_change_24h']
+                    self.data[sym]['dp']['percent_change_24h'] = fiat_quote['percent_change_24h']
 
-                    if self.data[sym]['prev_price'] != None:
-                        if self.data[sym]['quote_price'] > self.data[sym]['prev_price']:
+                    if self.data[sym]['prev_price'] is not None:
+                        if self.data[sym]['dp']['quote_price'] > self.data[sym]['prev_price']:
                             self.data[sym]['direction'] = 1
-                        elif self.data[sym]['quote_price'] < self.data[sym]['prev_price']:
+                        elif self.data[sym]['dp']['quote_price'] < self.data[sym]['prev_price']:
                             self.data[sym]['direction'] = -1
                         else:
                             self.data[sym]['direction'] = 0
 
-                        if len(self.data[sym]['history']) >= self.config['history_length']:
-                            self.data[sym]['history'].pop(0)
-                        self.data[sym]['history'].append(self.data[sym]['direction'])
+                        if self.config['history']['enabled']:
+                            if len(self.data[sym]['history']) >= self.config['history']['length']:
+                                self.data[sym]['history'].pop(0)
+                            self.data[sym]['history'].append(self.data[sym]['direction'])
 
-                    self.data[sym]['prev_price'] = self.data[sym]['quote_price']
+                    if self.data[sym]['prev_volume_24h'] is not None:
+                        self.data[sym]['diff_volume_24h'] = self.data[sym]['dp']['volume_24h'] - self.data[sym]['prev_volume_24h']
+                    self.data[sym]['prev_volume_24h'] = self.data[sym]['dp']['volume_24h']
+
+                    self.data[sym]['prev_price'] = self.data[sym]['dp']['quote_price']
             else:
                 raise ValueError(f'Unknown data provider: {dp["id"]}')
 
     def _screen_update(self):
-        # print('-> _screen_update()')
-
         # Jump to top left.
         print(JUMP_BEGINNING, end='', flush=True)
+
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'Last update: {now}')
+        print()
+        print('SYM     PRICE     24P%     24V%              24V           24V/LU')
+        print('-----------------------------------------------------------------')
 
         row_width = 0
         row_c = 0
@@ -125,10 +161,17 @@ class App():
             row_c += 1
 
             history = ''.join(list(map(_m, coin['history'])))
-            #out_t = '{:4s} {:>8.2f} {}'.format(sym, coin['quote_price'], history)
-            out_r = '{:4s} {:>8.2f}{} {}'.format(sym, coin['quote_price'], rs.all, history)
+            out_r = '{:4s} {:>8.2f}{} {:>8.2f} {:>8.2f} {:>16.2f} {:>16.2f} {}'.format(
+                sym,
+                coin['dp']['quote_price'],
+                rs.all,
+                coin['dp']['percent_change_24h'],
+                coin['dp']['volume_change_24h'],
+                coin['dp']['volume_24h'],
+                coin['diff_volume_24h'],
+                history
+            )
             row_width = max(row_width, len(out_r) - 3)
-            #print(f'-> len {len(out_t)} {len(out_r)}')
 
             if coin['direction'] == 1:
                 fg_color = fg.green
@@ -141,7 +184,7 @@ class App():
             print(row_s, end='', flush=True)
             sleep(0.1)
             print(f'\033[{column_base}G', end='', flush=True)
-            sleep(0.1)
+            sleep(0.05)
             print('\033[1B', end='', flush=True)
             sleep(0.1)
 
@@ -155,7 +198,8 @@ class App():
             if not self.running:
                 break
 
-        print(f'\033[1E', end='', flush=True)
+        print('\033[1E', end='', flush=True)
+
 
     def shutdown(self, reason: str):
         print()
@@ -165,7 +209,10 @@ class App():
     def _default_config(self):
         return {
             'update_interval': 60,
-            'history_length': 5,
+            'history': {
+                'enabled': False,
+                'length': 5,
+            },
             'convert': 'USD',
             'symbols': ['BTC'],
             'data_providers': [],
@@ -174,11 +221,17 @@ class App():
 def main():
     parser = argparse.ArgumentParser(prog='bittrackr', description='BitTrackr')
     parser.add_argument('-c', '--config', type=str, nargs=1, required=False, help='Path to Config File', default=[None])
+    parser.add_argument('-i', '--update-interval', type=int, nargs=1, required=False, help='Overwrite update_interval in config', default=[300])
+    parser.add_argument('-u', '--max-updates', type=int, nargs=1, required=False, help='Max Updates', default=[None])
 
     args = parser.parse_args()
     print(args)
 
-    app = App(args.config[0])
+    app = App(
+        args.config[0],
+        update_interval=args.update_interval[0],
+        max_updates=args.max_updates[0],
+    )
 
     signal.signal(signal.SIGINT, lambda sig, frame: app.shutdown('SIGINT'))
 
