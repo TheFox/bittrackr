aefc17b96aa725e8a2919d1da2ad8cbbd8c75427
Christian Mayer <christian@fox21.at>
2024-05-01 17:18:17 +0200

    Portfolio

--- src/bitportfolio.py
+++ src/bitportfolio.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import signal
-import argparse
+from argparse import ArgumentParser, BooleanOptionalAction
 from json import loads, dumps
 from cmc import get_quotes as cmc_get_quotes
 from sty import fg, bg, ef, rs
@@ -11,9 +11,12 @@ from pathlib import Path
 class App():
     config: dict
     running: bool
+    quotes: dict
 
     def __init__(self, config_path: str|None):
         print(f'-> config path: {config_path}')
+
+        self.quotes = {}
 
         if config_path is None:
             raise Exception('Config file not provided')
@@ -21,17 +24,17 @@ class App():
         with open(config_path, 'r') as f:
             self.config = loads(f.read())
 
-    def run(self, basedir: str):
+    def run(self, basedir: str, show_transactions: bool = False):
         self.running = True
 
         print(f'-> basedir: {basedir}')
         data = self._traverse(Path(basedir))
+        data = self._prepare(data)
 
-        unique_symbols = set()
-        for pair_id, pdata in data['pairs'].items():
-            unique_symbols.add(pdata['sell_symbol'])
-            unique_symbols.add(pdata['buy_symbol'])
-        self.quotes = self._get_quotes(unique_symbols)
+        symbols = data['buy_symbols']
+        print(f'-> symbols: {symbols}')
+        self.quotes = self._get_quotes(symbols)
+        data = self._calc_value(data)
 
         self._print_data(data)
 
@@ -43,37 +46,47 @@ class App():
     def _traverse(self, dir: Path, level: int = 0) -> dict:
         print(f'-> dir: {dir.name}')
         collection = {
-            'dir': dir.name,
+            'symbols': [],
+            'sell_symbols': [],
+            'buy_symbols': [],
+            'fees': 0.0,
+            'value': 0.0,
+            'locations': [],
             'pairs': {},
         }
         for file in dir.iterdir():
             if file.is_dir():
                 data = self._traverse(file, level + 1)
 
+                collection['symbols'].extend(data['symbols'])
+                collection['sell_symbols'].extend(data['sell_symbols'])
+                collection['buy_symbols'].extend(data['buy_symbols'])
+                collection['value'] += (data['value'])
+
                 for pair_id, pdata in data['pairs'].items():
                     if pair_id not in collection['pairs']:
                         collection['pairs'][pair_id] = {
                             'sell_symbol': pdata['sell_symbol'],
                             'buy_symbol': pdata['buy_symbol'],
-                            'symbols': [],
-                            'transactions': [],
-                            'prices': [],
                             'quantity': 0.0,
                             'fees': 0.0,
+                            'value': 0.0,
                             'locations': [],
+                            'transactions': [],
                         }
 
-                    collection['pairs'][pair_id]['symbols'].extend(pdata['symbols'])
+                    collection['fees'] += pdata['fees']
+                    collection['locations'].extend(pdata['locations'])
+
                     collection['pairs'][pair_id]['transactions'].extend(pdata['transactions'])
-
-                    collection['pairs'][pair_id]['prices'].extend(pdata['prices'])
-
                     collection['pairs'][pair_id]['quantity'] += pdata['quantity']
                     collection['pairs'][pair_id]['fees'] += pdata['fees']
-
                     collection['pairs'][pair_id]['locations'].extend(pdata['locations'])
 
             else:
+                if not str(file).endswith('.json'):
+                    continue
+
                 print(f'-> f: {file}')
                 with open(file, 'r') as f:
                     json = loads(f.read())
@@ -85,18 +98,27 @@ class App():
                     collection['pairs'][pair] = {
                         'sell_symbol': sell_symbol,
                         'buy_symbol': buy_symbol,
-                        'symbols': [],
-                        'transactions': [],
-                        'prices': [],
                         'quantity': 0.0,
                         'fees': 0.0,
+                        'value': 0.0,
                         'locations': [],
+                        'transactions': [],
                     }
 
-                collection['pairs'][pair]['symbols'].append(sell_symbol)
-                collection['pairs'][pair]['symbols'].append(buy_symbol)
+                collection['symbols'].append(sell_symbol)
+                collection['symbols'].append(buy_symbol)
+                collection['sell_symbols'].append(sell_symbol)
+                collection['buy_symbols'].append(buy_symbol)
 
                 for transaction in json['transactions']:
+                    # print('---------- transaction ----------')
+                    # print(transaction)
+                    # print('---------------------------------')
+
+                    transaction['cost'] = transaction['price'] * transaction['quantity']
+
+                    collection['fees'] += transaction['fee']
+
                     collection['pairs'][pair]['transactions'].append(transaction)
 
                     if transaction['type'] == 'buy':
@@ -105,41 +127,105 @@ class App():
                         collection['pairs'][pair]['quantity'] -= transaction['quantity']
 
                     collection['pairs'][pair]['fees'] += transaction['fee']
-                    collection['pairs'][pair]['locations'].append(transaction['location'])
+
+                    location = transaction.get('location')
+                    if location is not None:
+                        collection['pairs'][pair]['locations'].append(transaction['location'])
+                        collection['locations'].append(transaction['location'])
 
         return collection
 
+    def _prepare(self, data: dict) -> dict:
+        print('-> _prepare')
+
+        symbols = set()
+        for sym in data['symbols']:
+            symbols.add(sym)
+        data['symbols'] = list(symbols)
+
+        sell_symbols = set()
+        for sym in data['sell_symbols']:
+            sell_symbols.add(sym)
+        data['sell_symbols'] = list(sell_symbols)
+
+        buy_symbols = set()
+        for sym in data['buy_symbols']:
+            buy_symbols.add(sym)
+        data['buy_symbols'] = list(buy_symbols)
+
+        locations = set()
+        for sym in data['locations']:
+            locations.add(sym)
+        data['locations'] = list(locations)
+
+        for pid, pdata in data['pairs'].items():
+            locations = set()
+            for sym in pdata['locations']:
+                locations.add(sym)
+            data['pairs'][pid]['locations'] = list(locations)
+
+            transactions = sorted(pdata['transactions'], key=lambda t: t['date'])
+            data['pairs'][pid]['transactions'] = transactions
+
+        return data
+
     def _get_quotes(self, symbols: list) -> dict:
-        print('-> get_quotes')
+        print(f'-> _get_quotes({symbols})')
         cmc_config = self.config['data_providers'][0]
-        return cmc_get_quotes(
+        data = cmc_get_quotes(
             api_host=cmc_config['api']['host'],
             api_key=cmc_config['api']['key'],
             convert=self.config['convert'],
             symbols=symbols,
         )
+        # print('----------- data -----------')
+        # print(dumps(data, indent=2))
+        # print('----------------------------')
+        return data
+
+    def _calc_value(self, data: dict):
+        print('-> _calc_value')
+
+        for pid, pdata in data['pairs'].items():
+            quote = self.quotes['data'][pdata["buy_symbol"]][0]['quote'][self.config['convert']]['price']
+            data['pairs'][pid]['quote'] = quote
+
+            value = quote * pdata['quantity']
+            data['pairs'][pid]['value'] = value
+
+            transactions = []
+            for transaction in pdata['transactions']:
+                transaction['value'] = quote * transaction['quantity']
+                transactions.append(transaction)
+
+            data['pairs'][pid]['transactions'] = transactions
+
+        return data
 
     def _print_data(self, data: dict):
         print('------------')
-        print(dumps(self.quotes, indent=4))
-        print('------------')
-        print(dumps(data, indent=4))
-        print('------------')
+        print('SYM QUANTITY VALUE COST PROFIT')
 
         for pair_id, pdata in data['pairs'].items():
-            coin = self.quotes['data'][pdata['buy_symbol']]
-            print(f'-> pair: {pair_id}')
-            # print(f'-> symbols: {pdata["symbols"]}')
-            print(f'-> transactions: {len(pdata["transactions"])}')
-            print(f'-> quantity: {pdata["quantity"]}')
-            print(f'-> fees: {pdata["fees"]}')
+            print(dumps(pdata, indent=2))
+            # print(f'-> pair: {pair_id}')
+            # print(f'-> transactions: {len(pdata["transactions"])}')
+            # print(f'-> quantity: {pdata["quantity"]}')
+            # print(f'-> fees: {pdata["fees"]}')
             # print(f'-> locations: {pdata["locations"]}')
-            print()
+            # print(f'-> value: {pdata["value"]}')
+            row = '{} {}  {:.2f}'.format(
+                pdata['buy_symbol'],
+                pdata['quantity'],
+                pdata['value'],
+            )
+            print(row)
 
 def main():
-    parser = argparse.ArgumentParser(prog='bitportfolio', description='BitPortfolio')
+    parser = ArgumentParser(prog='bitportfolio', description='BitPortfolio')
     parser.add_argument('-c', '--config', type=str, nargs=1, required=False, help='Path to Config File', default=[None])
     parser.add_argument('-d', '--basedir', type=str, nargs=1, required=False, help='Path to directory', default=[None])
+    parser.add_argument('-t', '--transactions', action=BooleanOptionalAction, help='Show transactions', default=False)
 
     args = parser.parse_args()
     print(args)
@@ -149,7 +235,7 @@ def main():
     signal.signal(signal.SIGINT, lambda sig, frame: app.shutdown('SIGINT'))
 
     try:
-        app.run(args.basedir[0])
+        app.run(args.basedir[0], args.transactions)
     except KeyboardInterrupt:
         app.shutdown('KeyboardInterrupt')
 
